c0bcb5d15a0121e9f2347862a729a51268c69f91
Srikanth Sastry <srikanthsastry@gmail.com>
2025-04-01 06:17:39 -0700

    devserver changes

--- modules/utility/init.zsh
+++ modules/utility/init.zsh
@@ -123,7 +123,7 @@ alias lk='ll -Sr'        # Lists sorted by size, largest last.
 alias lt='ll -tr'        # Lists sorted by date, most recent last.
 alias lc='lt -c'         # Lists sorted by date, most recent last, shows change time.
 alias lu='lt -u'         # Lists sorted by date, most recent last, shows access time.
-alias sl='ls'            # I often screw this up.
+# alias sl='ls'            # I often screw this up.
 
 # Grep
 if zstyle -t ':prezto:module:utility:grep' color; then
